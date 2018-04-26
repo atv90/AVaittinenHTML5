70797fedbf0298ecf5f6585660761e25e5b0d15b
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-04-26 11:22:06 +0300

    Luotu uusia sivuja ja muokattu ulkonäköä.

---
+++ AVaittinenHTML5/Oma.js
@@ -0,0 +1,18 @@
+﻿
+$(document).ready(function () {
+    //piilotetaan kaikki sisältöalueet
+    $('.sisältö').hide();
+    
+    $('#ulkonäkö').on('click', function () {
+        //piilotetaan sisältö klikatessa
+        $('.sisältö').hide();
+        //näytetään näytetään uusi teksti klikatessa
+        $('#p1').show();
+    });
+    $('#levinneisyys').on('click', function () {
+        //piilotetaan sisältö klikatessa
+        $('.sisältö').hide();
+        //näytetään näytetään uusi teksti klikatessa
+        $('#p2').show();
+    });
+});
